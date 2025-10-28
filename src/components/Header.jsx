8d0ff3d64f149ea7a2866a8e592a8f33a05ae34a
All Fiction <126734865+ziqkimi308@users.noreply.github.com>
2025-10-28 14:51:30 +0800

    Updated Link to NavLink in Header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 
 const Header = () => {
 	return (
@@ -9,7 +9,7 @@ const Header = () => {
 						to="/"
 						className="flex items-center space-x-3 rtl:space-x-reverse"
 					>
-						<img src="logo.png" className="h-8" alt="Movie Finder Logo" />
+						<img src="/logo.png" className="h-8" alt="Movie Finder Logo" />
 						<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
 							Movie Finder App
 						</span>
@@ -121,37 +121,37 @@ const Header = () => {
 						</div>
 						<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 							<li>
-								<Link
-									to="#"
+								<NavLink
+									to="/"
 									className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-									aria-current="page"
+									end
 								>
 									Home
-								</Link>
+								</NavLink>
 							</li>
 							<li>
-								<Link
-									to="movies/popular"
-									className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+								<NavLink
+									to="/movies/popular"
+									className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 								>
 									Popular
-								</Link>
+								</NavLink>
 							</li>
 							<li>
-								<Link
+								<NavLink
 									to="movies/top"
 									className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 								>
-									Top
-								</Link>
+									Top Rated
+								</NavLink>
 							</li>
 							<li>
-								<Link
+								<NavLink
 									to="movies/upcoming"
 									className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 								>
 									Upcoming
-								</Link>
+								</NavLink>
 							</li>
 						</ul>
 					</div>
